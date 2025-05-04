ca8f4a9a1d9ecbadc5dfe4d3fd9b1b5b7c6ef8de
Eddiebear25 <edward7843@gmail.com>
2025-05-04 17:47:32 +0800

    Update PostHogPageView.tsx

--- src/components/analytics/PostHogPageView.tsx
+++ src/components/analytics/PostHogPageView.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import { usePathname, useSearchParams } from 'next/navigation';
 import { usePostHog } from 'posthog-js/react';
 import { Suspense, useEffect } from 'react';
@@ -9,28 +8,21 @@ const PostHogPageView = () => {
   const searchParams = useSearchParams();
   const posthog = usePostHog();
 
-  // Track pageviews
   useEffect(() => {
     if (pathname && posthog) {
       let url = window.origin + pathname;
-      if (searchParams?.toString()) {  // Optional chaining 추가
+      if (searchParams?.toString()) {
         url = `${url}?${searchParams.toString()}`;
       }
-
-      posthog.capture('$pageview', { $current_url: url });
+      posthog?.capture('$pageview', { $current_url: url });
     }
-  }, [pathname, searchParams, posthog]);
+  }, [pathname, searchParams?.toString(), posthog]);
 
   return null;
 };
 
-// Wrap this in Suspense to avoid the `useSearchParams` usage above
-// from de-opting the whole app into client-side rendering
-// See: https://nextjs.org/docs/messages/deopted-into-client-rendering
-export const SuspendedPostHogPageView = () => {
-  return (
-    <Suspense fallback={null}>
-      <PostHogPageView />
-    </Suspense>
-  );
-};
+export const SuspendedPostHogPageView = () => (
+  <Suspense fallback={null}>
+    <PostHogPageView />
+  </Suspense>
+);
